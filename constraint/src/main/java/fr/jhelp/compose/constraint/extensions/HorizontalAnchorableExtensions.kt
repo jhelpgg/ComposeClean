1e5cf6672651a8c4875acd8b834dd9386c41569e
JHelpGG <jhelpgg@gmail.com>
2022-10-09 14:16:00 +0200

    Constraint documentation

--- constraint/src/main/java/fr/jhelp/compose/constraint/extensions/HorizontalAnchorableExtensions.kt
+++ constraint/src/main/java/fr/jhelp/compose/constraint/extensions/HorizontalAnchorableExtensions.kt
@@ -6,11 +6,14 @@ import fr.jhelp.compose.constraint.theme.FAR
 import fr.jhelp.compose.constraint.theme.NEAR
 import fr.jhelp.compose.constraint.theme.NEXT
 
+/** Place component at default distance to other one in horizontal */
 infix fun HorizontalAnchorable.next(horizontalAnchor: ConstraintLayoutBaseScope.HorizontalAnchor) =
     this.linkTo(anchor = horizontalAnchor, margin = NEXT)
 
+/** Place component near to other one in horizontal */
 infix fun HorizontalAnchorable.near(horizontalAnchor: ConstraintLayoutBaseScope.HorizontalAnchor) =
     this.linkTo(anchor = horizontalAnchor, margin = NEAR)
 
+/** Place component far from other one in horizontal */
 infix fun HorizontalAnchorable.far(horizontalAnchor: ConstraintLayoutBaseScope.HorizontalAnchor) =
     this.linkTo(anchor = horizontalAnchor, margin = FAR)
--- constraint/src/main/java/fr/jhelp/compose/constraint/extensions/VerticalAnchorableExtensions.kt
+++ constraint/src/main/java/fr/jhelp/compose/constraint/extensions/VerticalAnchorableExtensions.kt
@@ -6,11 +6,14 @@ import fr.jhelp.compose.constraint.theme.FAR
 import fr.jhelp.compose.constraint.theme.NEAR
 import fr.jhelp.compose.constraint.theme.NEXT
 
+/** Place component at default distance to other one in vertical */
 infix fun VerticalAnchorable.next(VerticalAnchor: ConstraintLayoutBaseScope.VerticalAnchor) =
     this.linkTo(anchor = VerticalAnchor, margin = NEXT)
 
+/** Place component near to other one in vertical */
 infix fun VerticalAnchorable.near(VerticalAnchor: ConstraintLayoutBaseScope.VerticalAnchor) =
     this.linkTo(anchor = VerticalAnchor, margin = NEAR)
 
+/** Place component far from other one in vertical */
 infix fun VerticalAnchorable.far(VerticalAnchor: ConstraintLayoutBaseScope.VerticalAnchor) =
     this.linkTo(anchor = VerticalAnchor, margin = FAR)
