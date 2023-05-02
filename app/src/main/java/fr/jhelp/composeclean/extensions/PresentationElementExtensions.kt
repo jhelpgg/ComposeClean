9b6792ebcdf69ee19397acb239a448985698e5d9
JHelpGG <jhelpgg@gmail.com>
2023-05-02 08:16:31 +0200

    Nicer presentation list

---
+++ app/src/main/java/fr/jhelp/composeclean/extensions/PresentationElementExtensions.kt
@@ -0,0 +1,59 @@
+package fr.jhelp.composeclean.extensions
+
+import fr.jhelp.composeclean.R
+import fr.jhelp.composeclean.models.presentation.PresentationElement
+
+/**
+ * PresentationElement resource text
+ */
+val PresentationElement.resourceText: Int
+    get() =
+        when (this)
+        {
+            PresentationElement.CONTACT_LIST                -> R.string.contactList
+            PresentationElement.SOUNDS                      -> R.string.soundManager
+            PresentationElement.GREY_IMAGE                  -> R.string.greyImage
+            PresentationElement.TINT_IMAGE                  -> R.string.tintImage
+            PresentationElement.MASK_IMAGE                  -> R.string.maskImage
+            PresentationElement.SHIFT_IMAGE                 -> R.string.shiftImage
+            PresentationElement.CONTRAST_IMAGE              -> R.string.contrastImage
+            PresentationElement.MULTIPLY_IMAGE              -> R.string.multiplyImage
+            PresentationElement.ADD_IMAGE                   -> R.string.addImage
+            PresentationElement.DARKER_IMAGE                -> R.string.darkerImage
+            PresentationElement.INVERT_COLORS_IMAGE         -> R.string.invertColorImage
+            PresentationElement.BUMP_MAP_IMAGE              -> R.string.bumpImage
+            PresentationElement.NEON_LINES_DRAW             -> R.string.neonLinesDraw
+            PresentationElement.REPEAT_ON_LINE_DRAW         -> R.string.repeatOnLineDraw
+            PresentationElement.HELLO_WORLD_3D              -> R.string.helloWord3D
+            PresentationElement.MATERIAL_COLOR_3D           -> R.string.material3D
+            PresentationElement.MATERIAL_TEXTURE_3D         -> R.string.material3D
+            PresentationElement.CROSS_TEXTURE_3D            -> R.string.crossTexture
+            PresentationElement.TRANSPARENCY_3D             -> R.string.transparency
+            PresentationElement.SPHERE_3D                   -> R.string.sphere
+            PresentationElement.PLANE_3D                    -> R.string.plane
+            PresentationElement.REVOLUTION_3D               -> R.string.revolution
+            PresentationElement.FIELD_3D                    -> R.string.field
+            PresentationElement.DICE_3D                     -> R.string.dice
+            PresentationElement.ROBOT_3D                    -> R.string.robot
+            PresentationElement.TEDDY_BEAR_3D               -> R.string.teddyBear
+            PresentationElement.ANIMATION_INTERPOLATION_3D  -> R.string.animationAndInterpolation
+            PresentationElement.BACKGROUND_FOREGROUND_3D    -> R.string.backgroundForegroundIn3D
+            PresentationElement.SOUND_3D                    -> R.string.sound3D
+            PresentationElement.PARTICLE_EFFECT_EXPLOSION   -> R.string.particleEffectExplosion
+            PresentationElement.PARTICLE_EFFECT_SWORD_SLASH -> R.string.particleEffectSwordSlash
+            PresentationElement.OVER_GUI_3D                 -> R.string.overGUI
+            PresentationElement.VIRTUAL_JOYSTICK            -> R.string.virtualJoystick
+            PresentationElement.MINI_RPG                    -> R.string.miniRPG
+        }
+
+/**
+ * PresentationElement resource image
+ */
+val PresentationElement.resourceImage: Int
+    get() =
+        when (this)
+        {
+            PresentationElement.CONTACT_LIST -> R.drawable.contact_list_ui
+            PresentationElement.SOUNDS       -> R.drawable.sound_manager_ui
+            else                             -> android.R.drawable.ic_delete
+        }
--- app/src/main/java/fr/jhelp/composeclean/ui/composables/presentation/PresantationElementComposable.kt
+++ app/src/main/java/fr/jhelp/composeclean/ui/composables/presentation/PresantationElementComposable.kt
@@ -1,12 +1,18 @@
 package fr.jhelp.composeclean.ui.composables.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import fr.jhelp.composeclean.extensions.resourceImage
+import fr.jhelp.composeclean.extensions.resourceText
 import fr.jhelp.composeclean.models.presentation.PresentationElement
 
 /**
@@ -16,10 +22,15 @@ import fr.jhelp.composeclean.models.presentation.PresentationElement
 fun DrawPresentationElement(presentationElement: PresentationElement,
                             onClick: (PresentationElement) -> Unit)
 {
-    Text(text = "> ${presentationElement.screen} : ${presentationElement.presentationType}",
-         modifier = Modifier
-             .fillMaxWidth()
-             .wrapContentHeight()
-             .clickable { onClick(presentationElement) },
-         textAlign = TextAlign.Left)
+    val text = stringResource(id = presentationElement.resourceText)
+    Row {
+        Image(painter = painterResource(id = presentationElement.resourceImage),
+              contentDescription = text)
+        Text(text = text,
+             modifier = Modifier
+                 .fillMaxWidth()
+                 .wrapContentHeight()
+                 .clickable { onClick(presentationElement) },
+             textAlign = TextAlign.Left)
+    }
 }
