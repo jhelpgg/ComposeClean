125b2a3269dc10831e5ddb201ce10053f488a1fa
JHelpGG <jhelpgg@gmail.com>
2023-05-25 06:36:28 +0200

    Dealy extensions in functions

--- tasks/src/main/java/fr/jhelp/tasks/extensions/FunctionExtensions.kt
+++ tasks/src/main/java/fr/jhelp/tasks/extensions/FunctionExtensions.kt
@@ -20,3 +20,22 @@ fun <R : Any> (() -> R).parallel(taskType: TaskType = TaskType.SHORT_TASK): Futu
 fun <P : Any, R : Any> ((P) -> R).parallel(parameter: P,
                                            taskType: TaskType = TaskType.SHORT_TASK): FutureResult<R> =
     taskType.launch(parameter, this)
+
+/**
+ * Launch the function/lambda in parallel thread with a delay
+ *
+ * @return Future to be able to react on completion (success, failure or cancel)
+ */
+fun <R : Any> (() -> R).delay(delayInMilliseconds: Long,
+                              taskType: TaskType = TaskType.SHORT_TASK): FutureResult<R> =
+    taskType.delay(delayInMilliseconds, this)
+
+/**
+ * Launch the function/lambda in parallel thread with a delay
+ *
+ * @param parameter Parameter give at action when its effectively executed
+ * @return Future to be able to react on completion (success, failure or cancel)
+ */
+fun <P : Any, R : Any> ((P) -> R).delay(delayInMilliseconds: Long, parameter: P,
+                                        taskType: TaskType = TaskType.SHORT_TASK): FutureResult<R> =
+    taskType.delay(delayInMilliseconds, parameter, this)
