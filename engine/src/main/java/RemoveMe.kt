de6cc4decfa960637c2406937bb734200c466474
JHelpGG <jhelpgg@gmail.com>
2023-07-14 15:49:03 +0200

    3D engine documentation : Clone 3D

--- engine/src/main/java/RemoveMe.kt
+++ engine/src/main/java/RemoveMe.kt
@@ -1,3 +1,5 @@
+import fr.jhelp.android.library.engine.dsl.nodeReferences
+import fr.jhelp.android.library.engine.extensions.position
 import fr.jhelp.android.library.engine.extensions.tree
 import fr.jhelp.android.library.engine.view.View3D
 
@@ -5,13 +7,27 @@ fun sceneReady(view3D: View3D)
 {
     view3D.tree {
         this.root {
-            this.object3D {
-                this.addTriangle(0.2f, 5f, 3f,
-                                 0.1f, 0.2f,
-                                 8f, 4.1f, -5.2f,
-                                 0.2f, 0.3f,
-                                 7f, 8f, 3f,
-                                 .5f, 0.9f)
+            val (referenceObject, referenceClone, referenceClone2, referenceCloneClone) = nodeReferences()
+            this.object3D(referenceObject) {
+                // ...
+            }
+
+            this.clone(referenceClone, referenceObject) {
+                this.position {
+                    // ....
+                }
+            }
+
+            this.clone(referenceClone2, referenceObject) {
+                this.position {
+                    // ....
+                }
+            }
+
+            this.clone(referenceCloneClone, referenceClone) {
+                this.position {
+                    // ....
+                }
             }
         }
     }
