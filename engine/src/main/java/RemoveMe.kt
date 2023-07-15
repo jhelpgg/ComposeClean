ed6c790e843b1cc464a8f0202643895caf06f91e
JHelpGG <jhelpgg@gmail.com>
2023-07-15 16:06:39 +0200

    3D engine documentation : Robot

--- engine/src/main/java/RemoveMe.kt
+++ engine/src/main/java/RemoveMe.kt
@@ -1,22 +1,14 @@
 import fr.jhelp.android.library.engine.extensions.tree
+import fr.jhelp.android.library.engine.scene.geometry.robot.Robot
 import fr.jhelp.android.library.engine.view.View3D
-import fr.jhelp.android.library.math.PI_FLOAT
-import fr.jhelp.android.library.math.formal.X
-import fr.jhelp.android.library.math.formal.Y
-import fr.jhelp.android.library.math.formal.cos
-import fr.jhelp.android.library.math.formal.sin
-import fr.jhelp.android.library.math.formal.times
 
 fun sceneReady(view3D: View3D)
 {
+    val robot = Robot()
+
     view3D.tree {
         this.root {
-            this.field(
-                functionZ = cos(X) * sin(Y),
-                xStart = -PI_FLOAT, xEnd = PI_FLOAT, numberPartX = 10,
-                yStart = -PI_FLOAT, yEnd = PI_FLOAT, numberPartY = 10) {
-                // ...
-            }
+            this.node { this.add(robot.mainNode) }
         }
     }
 
