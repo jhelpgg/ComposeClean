e901d1ba7979005addddeb6b130ce2c67501550d
JHelpGG <jhelpgg@gmail.com>
2023-08-19 12:45:08 +0200

    Morphing triangle

---
+++ math/src/main/java/fr/jhelp/android/library/math/Triangle3D.kt
@@ -0,0 +1,9 @@
+package fr.jhelp.android.library.math
+
+/**
+ * Triangle in 3D space
+ * @property first First vertex
+ * @property second Second vertex
+ * @property third Third vertex
+ */
+data class Triangle3D(val first: Vertex, val second: Vertex, val third: Vertex)
---
+++ math/src/main/java/fr/jhelp/android/library/math/Vertex.kt
@@ -0,0 +1,8 @@
+package fr.jhelp.android.library.math
+
+/**
+ * Vertex represents a point in space with associated UV
+ * @property point3D Point in space
+ * @property uv Associated UV
+ */
+data class Vertex(val point3D: Point3D, val uv: Point2D)
